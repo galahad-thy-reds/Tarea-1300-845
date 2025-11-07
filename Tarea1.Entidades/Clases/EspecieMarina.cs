4861ce291101661e1180d291eff574eb854876b8
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-11-07 14:49:13 -0800

    Se agregan las entidades EspecieMarina y TipoEspecieMarina

---
+++ Tarea1.Entidades/Clases/EspecieMarina.cs
@@ -0,0 +1,30 @@
+﻿namespace Tarea.Entidades.Clases
+{
+    public class EspecieMarina
+    {
+        /// <summary>
+        /// Identificador unico de la especie
+        /// </summary>
+        public int Id { get; set; } = System.Random.Shared.Next(1, 1000000);
+        /// <summary>
+        /// Propiedad que indica el nombre de la especie marina
+        /// </summary>
+        public string? NombreEspecie { get; set; }
+        /// <summary>
+        /// Propiedad que indica el tipo de agua para la especie marina (agua dulce, agua salada)
+        /// </summary>
+        public string? TipoAgua { get; set; }
+        /// <summary>
+        /// Propiedad que indica si esta especie tiene aletas o no
+        /// </summary>
+        public bool TieneAletas { get; set; }
+        /// <summary>
+        /// Propiedad que indica si esta especie puede respirar fuera del agua o no
+        /// </summary>
+        public bool RespiraFueraDeAgua { get; set; }
+        /// <summary>
+        /// Peso, en kilos, que usalmente tiene la especie marina
+        /// </summary>
+        public double PesoKilos { get; set; }
+    }
+}
---
+++ Tarea1.Entidades/Clases/TipoEspecieMarina.cs
@@ -0,0 +1,18 @@
+﻿namespace Tarea.Entidades.Clases
+{
+    public class TipoEspecieMarina
+    {
+        /// <summary>
+        /// Identificador unico del tipo de especie marina
+        /// </summary>
+        public int Id { get; set; } = System.Random.Shared.Next(1, 1000000);
+        /// <summary>
+        /// Nombre del tipo de especie marina (Organismos Planctónicos, Organismos Nectónicos u Organismos Bentónicos)
+        /// </summary>
+        public string? NombreTipoEspecie { get; set; }
+        /// <summary>
+        /// Coleccion de especies marinas asociadas a este tipo de especie marina
+        /// </summary>
+        public IList<EspecieMarina>? EspeciesMarinas { get; set; }
+    }
+}
