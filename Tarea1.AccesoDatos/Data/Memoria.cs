5d9d5df5a92507abb2bc33a35cda9bcbd20b62c9
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-11-10 14:25:25 -0800

    Se utiliza NuGet para agregar el paquete de Caching Memory al proyecto

---
+++ Tarea1.AccesoDatos/Data/Memoria.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Reflection;
+using Tarea.Entidades.Clases;
+
+namespace Tarea.AccesoDatos.Data
+{
+    public class Memoria(IMemoryCache cache)
+    {
+        #region Atributos
+        private readonly IMemoryCache _cache = cache;
+        #endregion
+    }
+}
