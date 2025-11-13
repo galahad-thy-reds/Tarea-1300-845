2af38f03448e9c662ee46a980a75fa48684dded3
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-11-13 13:29:43 -0800

    Se agregan DataAnnotations a la mayoria de las propiedades. Se elimina la asociacion TipoEspecieMarina y EspecieMarina. Se modifica el id del TipoEspecieMarina para considerar el TipoOrganismo.

--- Tarea1.Entidades/Clases/EspecieMarina.cs
+++ Tarea1.Entidades/Clases/EspecieMarina.cs
@@ -1,4 +1,6 @@
-﻿namespace Tarea.Entidades.Clases
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea.Entidades.Clases
 {
     public class EspecieMarina
     {
@@ -9,22 +11,35 @@
         /// <summary>
         /// Propiedad que indica el nombre de la especie marina
         /// </summary>
+        [StringLength(100)]
+        [Required(ErrorMessage = "Por favor, indique el nombre de la especie marina")]
         public string? Nombre { get; set; }
         /// <summary>
         /// Propiedad que indica el tipo de agua para la especie marina (agua dulce, agua salada)
         /// </summary>
+        [StringLength(100)]
+        [Required(ErrorMessage = "Por favor, indique el tipo de agua para la especie marina (agua dulce, agua salada)")]
         public string? TipoDeAgua { get; set; }
         /// <summary>
         /// Propiedad que indica si esta especie tiene aletas o no
         /// </summary>
+        [Required(ErrorMessage = "Por favor, indique si la especie marina especie marina tiene aletas")]
         public bool TieneAletas { get; set; }
         /// <summary>
         /// Propiedad que indica si esta especie puede respirar fuera del agua o no
         /// </summary>
+        [Required(ErrorMessage = "Por favor, indique si la especie marina especie marina puede respirar fuera del agua")]
         public bool RespiraFueraDeAgua { get; set; }
         /// <summary>
         /// Peso, en kilos, que usalmente tiene la especie marina
         /// </summary>
+        [Range(0.01, double.MaxValue)]
+        [Required(ErrorMessage = "Por favor, indique el peso (en kilogramos) que usalmente tiene la especie marina")]
         public double PesoKilos { get; set; }
+        /// <summary>
+        ///  Identificador unico del tipo de especie marina.
+        /// </summary>
+        [Required(ErrorMessage = "Por favor, indique el tipo de especie marina de la especie")]
+        public TipoEspecieMarina? Tipo { get; set; }
     }
 }
--- Tarea1.Entidades/Clases/TipoEspecieMarina.cs
+++ Tarea1.Entidades/Clases/TipoEspecieMarina.cs
@@ -1,22 +1,26 @@
-﻿namespace Tarea.Entidades.Clases
+﻿using System.ComponentModel.DataAnnotations;
+using Tarea.Entidades.Enums;
+
+namespace Tarea.Entidades.Clases
 {
     public class TipoEspecieMarina
     {
         /// <summary>
-        /// Identificador unico del tipo de especie marina.
+        /// Identificador unico del tipo de especie marina utilizando el tipo fijo de organismo marino.
         /// </summary>
-        public int Id { get; set; } = System.Random.Shared.Next(1, 1000000);
+        [Required(ErrorMessage = "Por favor, indique el tipo de organismo")]
+        public TipoOrganismo Id { get; set; }
         /// <summary>
         /// Nombre del tipo de especie marina (Organismos Planctónicos, Organismos Nectónicos u Organismos Bentónicos).
         /// </summary>
+        [StringLength(100)]
+        [Required(ErrorMessage = "Por favor, indique el nombre del tipo de especie marina")]
         public string? Nombre { get; set; }
         /// <summary>
         /// Descripcion del tipo de especie marina.
         /// </summary>
+        [StringLength(250)]
+        [Required(ErrorMessage = "Por favor, indique la descripcion del tipo de especie marina")]
         public string? Descripcion { get; set; }
-        /// <summary>
-        /// Coleccion de especies marinas asociadas a este tipo de especie marina.
-        /// </summary>
-        public IList<EspecieMarina>? EspeciesMarinas { get; set; }
     }
 }
