905a46a379453957286fd5910714226c04465f01
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-11-13 09:25:38 -0800

    Se agrega enum TipoOrganismo para clasificar los tipos Se elimina la estructura de carpetas Servicios

---
+++ Tarea1.Entidades/Enums/TipoOrganismo.cs
@@ -0,0 +1,23 @@
+﻿namespace Tarea.Entidades.Enums
+{
+    /// <summary>
+    /// Representa los tipos fijos de organismos marinos.
+    /// </summary>
+    public enum TipoOrganismo
+    {
+        /// <summary>
+        /// Flotan en la superficie (fitoplancton, zooplancton).
+        /// </summary>
+        OrganismosPlanctonicos = 1,
+
+        /// <summary>
+        /// Viven en aguas libres (peces, ballenas, calamares).
+        /// </summary>
+        OrganismosNectonicos = 2,
+
+        /// <summary>
+        /// Viven en el fondo marino (algas, moluscos, corales).
+        /// </summary>
+        OrganismosBentonicos = 3
+    }
+}
